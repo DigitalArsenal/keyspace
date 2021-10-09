fa498dc1444df4c2df1ba58ef9645e768593c288
tj <tj@digitalarsenal.io>
2021-10-09 16:33:58 -0400

    updates

--- src/App.svelte
+++ src/App.svelte
@@ -1,33 +1,11 @@
 <script lang="ts">
+  import SideNav from "./SideNav.widget/SideNav.svelte";
   import Grid from "./Grid.widget/Grid.svelte";
 </script>
 
-<div class="container max-w-full w-full h-full grid grid-rows-2">
-  <nav class="bg-blue-900 shadow-lg">
-    <div class="container mx-auto">
-      <div class="sm:flex">
-        <a href={"#"} class="text-white text-3xl font-bold p-3">KEYSPACE</a>
-
-        <!-- Menus -->
-        <div class="ml-55 mt-4">
-          <ul class="text-white sm:self-center text-xl">
-            <li class="sm:inline-block">
-              <a href={"#"} class="p-3 hover:text-red-900">Account</a>
-            </li>
-            <li class="sm:inline-block">
-              <a href={"#"} class="p-3 hover:text-red-900">Peers</a>
-            </li>
-            <li class="sm:inline-block">
-              <a href={"#"} class="p-3 hover:text-red-900">Data</a>
-            </li>
-            <li class="sm:inline-block">
-              <a href={"#"} class="p-3 hover:text-red-900">FAQ</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </nav>
+<div
+  class="container max-w-full w-full max-h-full h-full h-screen grid grid-top-row">
+  <SideNav />
   <main>
     <Grid cssString="height:100%" grid data={[{ test: "a" }]} />
   </main>
@@ -38,6 +16,9 @@
   @tailwind components;
   @tailwind utilities;
 
+  .grid-top-row {
+    grid-template-columns: min-content auto;
+  }
   main {
     text-align: center;
   }
---
+++ src/SideNav.widget/SideNav.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Icon from "svelte-awesome";
+  import { user, addressBook, database } from "svelte-awesome/icons";
+
+  let menuItems = [{ icon: user }, { icon: addressBook }, { icon: database }];
+</script>
+
+<div
+  class="border-r-1h border-gray-600 flex flex-col pt-5 gap-5 justify-start bg-blue-500 w-12">
+  {#each menuItems as item, m}
+    <div
+      class="h-12 w-12 flex justify-center align-center items-center text-white cursor-pointer">
+      <Icon class="w-max" scale={2} data={item.icon} />
+    </div>
+  {/each}
+</div>
