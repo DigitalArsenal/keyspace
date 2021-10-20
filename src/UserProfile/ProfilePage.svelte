8dd8f3b211df9cc79bff3677ac3e47e800a6e4da
TJKoury <tj@digitalarsenal.io>
2021-10-20 14:51:57 -0400

    deploy

--- src/UserProfile/ProfilePage.svelte
+++ src/UserProfile/ProfilePage.svelte
@@ -1,18 +1,29 @@
 <script lang="ts">
   import { Buffer } from "buffer";
   import { masterNode } from "../stores/userprofile.store";
-  import createKeccakHash from "keccak";
-  import { toChecksumAddress } from "ethereum-checksum-address";
+  import { HD } from "ethers";
 
   let { payments } = globalThis.bitcoinjs;
 
   let btcAddress, btcSegWitAddress, ethAddress;
 
-  let api_servers = ["https://api.blockcypher.com/v1/btc/main/addrs/", "https://blockchain.info/rawaddr/"];
+  let api_servers = [
+    "https://api.blockcypher.com/v1/btc/main/addrs/",
+    "https://blockchain.info/rawaddr/"
+  ];
 
-  masterNode.subscribe(async (kP) => {
-    let bip44Account = $masterNode.deriveHardened(44).deriveHardened(0).deriveHardened(0).derive(0).derive(0);
-    const { address } = payments.p2pkh({ pubkey: bip44Account.publicKey, network: globalThis.bitcoinjs.bitcoin });
+  masterNode.subscribe(async mN => {
+    if (!mN) return;
+    let bip44Account = mN
+      .deriveHardened(44)
+      .deriveHardened(0)
+      .deriveHardened(0)
+      .derive(0)
+      .derive(0);
+    const { address } = payments.p2pkh({
+      pubkey: bip44Account.publicKey,
+      network: globalThis.bitcoinjs.bitcoin
+    });
     btcAddress = address;
     /*try {
       console.log(
@@ -21,34 +32,54 @@
           .catch((e) => {})
       );
     } catch (e) {}*/
-    let bip84Account = $masterNode.deriveHardened(84).deriveHardened(0).deriveHardened(0).derive(0).derive(0);
-    const { address: swaddress } = payments.p2wpkh({ pubkey: bip84Account.publicKey, network: globalThis.bitcoinjs.bitcoin });
+    let bip84Account = mN
+      .deriveHardened(84)
+      .deriveHardened(0)
+      .deriveHardened(0)
+      .derive(0)
+      .derive(0);
+    const { address: swaddress } = payments.p2wpkh({
+      pubkey: bip84Account.publicKey,
+      network: globalThis.bitcoinjs.bitcoin
+    });
     btcSegWitAddress = swaddress;
 
     ethAddress = "TODO"; //toChecksumAddress(`${keccakHex.substring(keccakHex.length - 40, keccakHex.length).toUpperCase()}`);
   });
 </script>
 
+<style>
+
+</style>
+
 <div class="text-2xl mb-10">ADDRESS</div>
 <h1 class="mb-5">
   BTC:
-  <a class="text-blue-600" href="https://www.blockchain.com/btc/address/{btcAddress}"> {btcAddress}</a>
+  <a
+    class="text-blue-600"
+    href="https://www.blockchain.com/btc/address/{btcAddress}">
+    {btcAddress}
+  </a>
 </h1>
 <h1 class="mb-5">
   BTC (SegWit):
-  <a class="text-blue-600" href="https://www.blockchain.com/btc/address/{btcSegWitAddress}"> {btcSegWitAddress}</a>
+  <a
+    class="text-blue-600"
+    href="https://www.blockchain.com/btc/address/{btcSegWitAddress}">
+    {btcSegWitAddress}
+  </a>
 </h1>
 <h1 class="mb-5">
   ETH:
-  <a class="text-blue-600" href="https://etherscan.io/address/{ethAddress}"> {ethAddress}</a>
+  <a class="text-blue-600" href="https://etherscan.io/address/{ethAddress}">
+    {ethAddress}
+  </a>
 </h1>
 <button
-  class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mt-10"
-  on:click={(e) => {
+  class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded
+  mt-10"
+  on:click={e => {
     $masterNode = null;
-  }}
->
+  }}>
   Clear
 </button>
-
-<style></style>
