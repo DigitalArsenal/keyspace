e834806a32b350dae18fb407ffa691b5ce125ffe
tjkoury <tjkoury@gmail.com>
2021-11-19 16:03:25 -0500

    update with eccrypto-js

--- src/MainPage/MainPage.svelte
+++ src/MainPage/MainPage.svelte
@@ -1,5 +1,42 @@
+<script>
+  import { onMount } from "svelte";
+  let eccrypto = {};
+
+  onMount(async () => {
+    // required to do after mount
+    eccrypto = await import("eccrypto-js");
+  });
+
+  let doCrypto = () => {
+    var privateKeyA = eccrypto.generatePrivate();
+    var publicKeyA = eccrypto.getPublic(privateKeyA);
+    var privateKeyB = eccrypto.generatePrivate();
+    var publicKeyB = eccrypto.getPublic(privateKeyB);
+    console.log(privateKeyA);
+    // Encrypting the message for B.
+    eccrypto
+      .encrypt(publicKeyB, Buffer.from("msg to b"))
+      .then(function (encrypted) {
+        // B decrypting the message.
+        eccrypto.decrypt(privateKeyB, encrypted).then(function (plaintext) {
+          console.log("Message to part B:", plaintext.toString());
+        });
+      });
+
+    // Encrypting the message for A.
+    eccrypto
+      .encrypt(publicKeyA, Buffer.from("msg to a"))
+      .then(function (encrypted) {
+        // A decrypting the message.
+        eccrypto.decrypt(privateKeyA, encrypted).then(function (plaintext) {
+          console.log("Message to part A:", plaintext.toString());
+        });
+      });
+  };
+</script>
+
 <div class="flex flex-col h-full items-center justify-center">
-  <div class="text-4xl">KEYSPACE.APP</div>
-  <hr>
+  <div class="text-4xl" on:click={doCrypto}>KEYSPACE.APP</div>
+  <hr />
   <div class="text-xl mt-2">Fully Decentralized PKI</div>
 </div>
--- vite.config.mjs
+++ vite.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { viteSingleFile } from "vite-plugin-singlefile";
 import postcss from "./postcss.config.mjs";
+import polyfill from "rollup-plugin-polyfill-node";
 
 export default defineConfig({
   plugins: [svelte(), viteSingleFile()],
